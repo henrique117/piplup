0f38848a887e3840784e21afb5ded62a2b08cb55
Henrique <hassis.moreira177@hotmail.com>
2025-01-15 00:36:37 -0300

    deu n

--- dist/auxiliarfunctions/playerEmbedBuilder.js
+++ dist/auxiliarfunctions/playerEmbedBuilder.js
@@ -2,28 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const dbQuerys_1 = require("../database/dbQuerys");
-const axios_1 = require("axios");
-const sharp = require("sharp");
-const fs = require("fs");
-const path = require("path");
-async function playerEmbedBuilder(player) {
-    const imagePath = path.resolve(__dirname, `assets/pfp.${player.player_pfp.split('.').pop()}`);
-    try {
-        const response = await axios_1.default.get(player.player_pfp, { responseType: 'arraybuffer' });
-        await sharp(response.data).resize(256, 256).toFile(imagePath);
-    }
-    catch (err) {
-        console.error('Error on resizing image');
-        throw new Error('Error on resizing image');
-    }
+async function shopEmbedBuilder(player) {
     const owner = player.user_id ? (await (0, dbQuerys_1.findUser)(player.user_id)).user_username : 'No one';
-    const playerEmbed = new discord_js_1.EmbedBuilder()
+    return new discord_js_1.EmbedBuilder()
         .setColor('Aqua')
         .setTitle(`:flag_${player.player_flag.toLowerCase()}: ${player.player_name} (#${player.player_rank})`)
         .setDescription(`**Value: ${player.player_cost}** :coin:\n\nOwner: ${owner}\nPlayer ID: ${player.player_id}`)
-        .setImage(`${imagePath}`);
-    if (fs.existsSync(imagePath))
-        fs.unlinkSync(imagePath);
-    return playerEmbed;
+        .setImage(`${player.player_pfp}`);
 }
-exports.default = playerEmbedBuilder;
+exports.default = shopEmbedBuilder;
--- src/auxiliarfunctions/playerEmbedBuilder.ts
+++ src/auxiliarfunctions/playerEmbedBuilder.ts
@@ -1,34 +1,14 @@
 import { EmbedBuilder } from 'discord.js'
 import { PlayerInterface } from '../interfaces/interfaces.export'
 import { findUser } from '../database/dbQuerys'
-import Axios from 'axios'
-import * as sharp from 'sharp'
-import * as fs from 'fs'
-import * as path from 'path'
 
-export default async function playerEmbedBuilder(player: PlayerInterface): Promise<EmbedBuilder> {
-
-    const imagePath = path.resolve(__dirname, `assets/pfp.${player.player_pfp.split('.').pop()}`)
-
-    try {
-        const response = await Axios.get<Buffer>(player.player_pfp, { responseType: 'arraybuffer' })
-        
-        await sharp(response.data).resize(256, 256).toFile(imagePath)
-
-    } catch (err) {
-        console.error('Error on resizing image')
-        throw new Error('Error on resizing image')
-    }
+export default async function shopEmbedBuilder(player: PlayerInterface): Promise<EmbedBuilder> {
 
     const owner = player.user_id ? (await findUser(player.user_id)).user_username : 'No one'
 
-    const playerEmbed = new EmbedBuilder()
+    return new EmbedBuilder()
         .setColor('Aqua')
         .setTitle(`:flag_${player.player_flag.toLowerCase()}: ${player.player_name} (#${player.player_rank})`)
         .setDescription(`**Value: ${player.player_cost}** :coin:\n\nOwner: ${owner}\nPlayer ID: ${player.player_id}`)
-        .setImage(`${imagePath}`)
-
-    if(fs.existsSync(imagePath)) fs.unlinkSync(imagePath)
-
-    return playerEmbed
+        .setImage(`${player.player_pfp}`)
 }
