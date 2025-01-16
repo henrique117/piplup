bcfed85093e0504e367e95d379d8bbed67a37adb
Henrique <hassis.moreira177@hotmail.com>
2025-01-15 22:58:20 -0300

    slash SA update

--- dist/slashcommands/sellall.js
+++ dist/slashcommands/sellall.js
@@ -5,7 +5,10 @@ const functions_export_1 = require("../functions/functions.export");
 module.exports = {
     data: new discord_js_1.SlashCommandBuilder()
         .setName('sellall')
-        .setDescription('Sell all of your players'),
+        .setDescription('Sell all of your players')
+        .addStringOption(option => option.setName('query')
+        .setDescription("IDs or names of the players you don't wanna sell")
+        .setRequired(true)),
     async execute(interaction) {
         await (0, functions_export_1.sellall)(interaction);
     }
