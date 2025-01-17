b78dd3ab60b7006af9d25f5b716a200732cb29eb
Henrique <hassis.moreira177@hotmail.com>
2025-01-17 01:04:31 -0300

    se for isso eu vou ficar doido

--- dist/auxiliarfunctions/playerEmbedBuilder.js
+++ dist/auxiliarfunctions/playerEmbedBuilder.js
@@ -3,13 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const dbQuerys_1 = require("../database/dbQuerys");
 async function playerEmbedBuilder(player) {
-    const player_db = await (0, dbQuerys_1.findPlayer)(player.player_name);
-    console.log('player_db:', player_db.user_id); // Depuração
+    const player_db = await (0, dbQuerys_1.findPlayer)(player.player_name.toLowerCase());
     let owner = 'No one';
     if (player_db && player_db.user_id) {
-        console.log('user_id found:', player_db.user_id); // Depuração
+        console.log('user_id found:', player_db.user_id);
         const user_db = await (0, dbQuerys_1.findUser)(player_db.user_id);
-        console.log('user_db:', user_db); // Depuração
+        console.log('user_db:', user_db);
         if (user_db) {
             owner = user_db.user_globalName ? user_db.user_globalName : user_db.user_username;
         }
--- src/auxiliarfunctions/playerEmbedBuilder.ts
+++ src/auxiliarfunctions/playerEmbedBuilder.ts
@@ -4,15 +4,14 @@ import { findPlayer, findUser } from '../database/dbQuerys'
 
 export default async function playerEmbedBuilder(player: PlayerInterface): Promise<EmbedBuilder> {
 
-    const player_db = await findPlayer(player.player_name)
-    console.log('player_db:', player_db.user_id); // Depuração
+    const player_db = await findPlayer(player.player_name.toLowerCase())
 
     let owner: string = 'No one'
 
     if(player_db && player_db.user_id) {
-        console.log('user_id found:', player_db.user_id); // Depuração
+        console.log('user_id found:', player_db.user_id)
         const user_db = await findUser(player_db.user_id)
-        console.log('user_db:', user_db); // Depuração
+        console.log('user_db:', user_db)
 
         if(user_db) {
             owner = user_db.user_globalName ? user_db.user_globalName : user_db.user_username
